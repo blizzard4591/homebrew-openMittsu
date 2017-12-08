822d606927e7a9b4f2c90d5a0f51e97f43e8fe55
PBerger <philipp.berger@rwth-aachen.de>
2017-12-08 01:02:53 +0100

    Initial formula commit.

---
+++ Formula/openmittsu.rb
@@ -0,0 +1,32 @@
+class OpenMittsu < Formula
+  desc "A cross-platform open source implementation and desktop client for the Threema Messenger App."
+  homepage "https://github.com/blizzard4591/openMittsu"
+  url "https://github.com/blizzard4591/openMittsu/archive/0.9.10.zip"
+  version "0.9.10"
+  sha256 "44a0973cd9d392a8c89f990e49c8a6a4e27157468c869a17e267e7852f80e799"
+  head "https://github.com/blizzard4591/openMittsu.git", :using => :git, :shallow => false
+
+  depends_on :macos => :mavericks
+  depends_on "cmake"
+  depends_on "libsodium"
+  depends_on "qt5"
+  depends_on "sqlcipher"
+  depends_on "qrencode"
+  depends_on "blizzard4591/homebrew-qt5-sqlcipher"
+
+  def install
+    args = %w[
+      -DOPENMITTSU_DEBUG=OFF
+      -DCMAKE_BUILD_TYPE=RELEASE
+    ]
+
+    mktemp do
+      system "cmake", buildpath, *(std_cmake_args + args)
+      system "make", "install"
+    end
+  end
+
+  test do
+    shell_output("#{bin}/openMittsuTests", 1)
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # homebrew-openMittsu
 A homebrew tap/formula for openMittsu, an open source implementation and desktop client of the Threema Messenger App.
+
+# Installing openMittsu via homebrew
+
+This tap provides the formula to install openMittsu via homebrew. All you need to do is
+
+```sh
+brew tap blizzard4591/homebrew-openMittsu
+brew install openmittsu
+``` 
+
+After the installation, you should be able to invoke openMittsu by just typing
+
+```sh
+openMittsu
+```
+
+For instructions on how to use openMittsu, we refer to [openMittsu's README](https://github.com/blizzard4591/openMittsu/blob/master/README.md).
