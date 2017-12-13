780acdbdf4ea3f871af90a5681a6832ce155504b
P. Berger <admin@philippberger.de>
2017-12-13 14:20:34 +0100

    Update openmittsu.rb

--- Formula/openmittsu.rb
+++ Formula/openmittsu.rb
@@ -1,9 +1,9 @@
 class Openmittsu < Formula
   desc "A cross-platform open source implementation and desktop client for the Threema Messenger App."
   homepage "https://github.com/blizzard4591/openMittsu"
-  url "https://github.com/blizzard4591/openMittsu/archive/0.9.10.zip"
-  version "0.9.10"
-  sha256 "44a0973cd9d392a8c89f990e49c8a6a4e27157468c869a17e267e7852f80e799"
+  url "https://github.com/blizzard4591/openMittsu/archive/0.9.11.zip"
+  version "0.9.11"
+  sha256 "617104cbc9b14a9338acf38b14bada15d042471f4bc554f5ae43c1313acec48c"
   head "https://github.com/blizzard4591/openMittsu.git", :using => :git, :shallow => false
 
   depends_on :macos => :mavericks
