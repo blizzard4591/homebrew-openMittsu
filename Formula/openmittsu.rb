810e02d4a5930ac06edaf2b6648f08a41c18d8cc
Philipp Berger <berger@cs.rwth-aachen.de>
2020-02-20 18:05:23 +0100

    Bump!

--- Formula/openmittsu.rb
+++ Formula/openmittsu.rb
@@ -1,13 +1,14 @@
 class Openmittsu < Formula
   desc "Cross-platform GPLv2 desktop client for the Threema Messenger App"
   homepage "https://github.com/blizzard4591/openMittsu"
-  url "https://github.com/blizzard4591/openMittsu/archive/0.9.14.tar.gz"
-  sha256 "d7bf4bbe7831a06da373fb0172e00d0794ead5564eda017f8dcd8f4f055fc6a9"
+  url "https://github.com/blizzard4591/openMittsu/archive/0.9.15.tar.gz"
+  sha256 "7c70de775716f8c01c6f6f4c812fd938f2aea979f6fe4562bb6a4089fa3b9cae"
   head "https://github.com/blizzard4591/openMittsu.git", :using => :git, :shallow => false
 
   depends_on "blizzard4591/homebrew-qt5-sqlcipher/qt5-sqlcipher"
   depends_on "cmake"
   depends_on "libsodium"
+  depends_on "spdlog"
   depends_on :macos => :mavericks
   depends_on "qrencode"
   depends_on "qt"
