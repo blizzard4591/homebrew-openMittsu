ea07d43a045e7ba8e3d72f692beee1bbdb44786d
Philipp Berger <berger@cs.rwth-aachen.de>
2020-02-20 17:29:55 +0100

    More audit fixes.

--- Formula/openmittsu.rb
+++ Formula/openmittsu.rb
@@ -1,16 +1,15 @@
 class Openmittsu < Formula
-  desc "Cross-platform open source implementation and desktop client for the Threema Messenger App"
+  desc "Cross-platform GPLv2 desktop client for the Threema Messenger App"
   homepage "https://github.com/blizzard4591/openMittsu"
   url "https://github.com/blizzard4591/openMittsu/archive/0.9.14.tar.gz"
-  version "0.9.14"
   sha256 "d7bf4bbe7831a06da373fb0172e00d0794ead5564eda017f8dcd8f4f055fc6a9"
   head "https://github.com/blizzard4591/openMittsu.git", :using => :git, :shallow => false
 
+  depends_on "blizzard4591/homebrew-qt5-sqlcipher/qt5-sqlcipher"
   depends_on "cmake"
   depends_on "libsodium"
-  depends_on "qrencode"
-  depends_on "blizzard4591/homebrew-qt5-sqlcipher/qt5-sqlcipher"
   depends_on :macos => :mavericks
+  depends_on "qrencode"
   depends_on "qt"
 
   def install
